6d8f4b51d378048244d17e87c7776d9e8c42ac99
Daniel <2raederdev@gmail.com>
2019-11-25 12:38:17 +0100

    Seed together

--- bin/seeds.js
+++ bin/seeds.js
@@ -5,7 +5,8 @@
 
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
-const User = require("../models/User");
+const User = require("../models/User.model");
+const Event = require("../models/Event.model")
 
 const bcryptSalt = 10;
 
@@ -36,6 +37,70 @@ User.deleteMany()
 .then(usersCreated => {
   console.log(`${usersCreated.length} users created with the following id:`);
   console.log(usersCreated.map(u => u._id));
+})
+.then(() => {
+  // Close properly the connection to Mongoose
+  mongoose.disconnect()
+})
+.catch(err => {
+  mongoose.disconnect()
+  throw err
+})
+
+let events = [
+  {
+    name: "Dinner in Rom",
+    description: "Dinner, Italian dinner!!!",
+    specs: {
+      glutenFree: false,
+      dairyFree: false,
+      veg: false,
+      vegan: false, 
+      shelfish: false,
+      nuts: true,
+    },
+    type: "dinner",
+    date: 12/12/2019,
+    time: "21:00",
+    location: {
+      address: "Calle Pez",
+      addressnumber: 9,
+      city: "Madrid"
+    },
+    forks: 4
+  },
+  {
+    name: "First lunch",
+    description: "Let's eat together!!!",
+    specs: {
+      glutenFree: false,
+      dairyFree: false,
+      veg: true,
+      vegan: false, 
+      shelfish: false,
+      nuts: true,
+    },
+    type: "lunch",
+    date: 12/12/2019,
+    time: "13:00",
+    location: {
+      address: "Calle Alcalá",
+      addressnumber: 12,
+      city: "Madrid"
+    },
+    forks: 6
+  },
+  
+]
+
+
+Event.deleteMany()
+.then(() => {
+  return Event.create(events)
+})
+.then(usersCreated => {
+  console.log(`${eventsCreated.length} event created with the following id:`);
+  console.log(eventsCreated.map(u => u._id));
 })
 .then(() => {
   // Close properly the connection to Mongoose
