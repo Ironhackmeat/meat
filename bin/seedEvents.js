9c17e5a180cbdbae971b59ba841a8502f9176865
Daniel <2raederdev@gmail.com>
2019-11-25 12:03:21 +0100

    Model y Seed de los events

---
+++ bin/seedEvents.js
@@ -0,0 +1,45 @@
+let events = [
+  {
+    name: "Dinner in Rom",
+    description: "Dinner, Italian dinner!!!",
+    specs: {
+      glutenFree: false,
+      dairyFree: false,
+      veg: false,
+      vegan: false, 
+      shelfish: false,
+      nuts: true,
+    },
+    type: "dinner",
+    date: 12/12/2019,
+    time: "21:00",
+    location: {
+      address: "Calle Pez",
+      addressnumber: 9,
+      city: "Madrid"
+    },
+    forks: 4
+  },
+  {
+    name: "First lunch",
+    description: "Let's eat together!!!",
+    specs: {
+      glutenFree: false,
+      dairyFree: false,
+      veg: true,
+      vegan: false, 
+      shelfish: false,
+      nuts: true,
+    },
+    type: "lunch",
+    date: 12/12/2019,
+    time: "13:00",
+    location: {
+      address: "Calle Alcalá",
+      addressnumber: 12,
+      city: "Madrid"
+    },
+    forks: 6
+  },
+  
+]
---
+++ models/Event.model.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+// Aquí el esquema
+
+const eventSchema = new Schema(
+    {
+        name: String,
+        description: String,
+        host: { type: Schema.Types.ObjectId, ref: 'User' },
+        specs: {
+          glutenfree: Boolean,
+          dairyfree: Boolean,
+          veg: Boolean,
+          vegan: Boolean, 
+          shellfish: Boolean,
+          nuts: Boolean,
+        },
+        type: {
+          type: String,
+          enum: [breakfast, brunch, lunch, dinner]
+        },
+        status: {
+          type: String,
+          enum: [open, closed],
+          default: open,
+        },
+        date: Date,
+        time: String,
+        recipeName: String,
+        recipePath: String,
+        imgName:  String,
+        imgPath: String, 
+        location: {
+          address: String,
+          addressnumber: Number,
+          city: String
+        },
+        forks: Number,
+        guests: [{ type: Schema.Types.ObjectId, ref: 'User' }]
+    },
+    {
+        timestamps: true
+    },
+    
+)
+
+module.exports = mongoose.model('Event', eventSchema)
