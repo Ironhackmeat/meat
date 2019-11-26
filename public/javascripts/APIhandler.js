91f312e84b5291c3c247c54d0f5f1e4a275123f4
Rebecca Wood <rebecca@Rebeccas-MacBook-Air.local>
2019-11-26 11:29:34 +0100

    signup updated

--- public/javascripts/APIhandler.js
+++ public/javascripts/APIhandler.js
@@ -4,14 +4,42 @@ class APIHandler {
   }
 
   getFullList() {
-    return axios.get(this.BASE_URL + "?q=all&app_id=edb96150&app_key=8a70cb8c5186d89d4e451e5f99236bb3&offset=100")
+    return axios.get(this.BASE_URL + "?q=all&app_id=edb96150&app_key=8a70cb8c5186d89d4e451e5f99236bb3&from=0&to=100")
       .then(response => response.data.hits)
 
   }
-  getFilters() {
-    return axios.post(this.BASE_URL )
+
+  getFiltersGF() {
+    return axios.get(this.BASE_URL + "?q=health=gluten-free&app_id=edb96150&app_key=8a70cb8c5186d89d4e451e5f99236bb3&from=0&to=100")
+      .then(response => response.data.hits)
+  }
+
+  getFiltersVeg() {
+    return axios.get(this.BASE_URL + "?q=health=vegetarian&app_id=edb96150&app_key=8a70cb8c5186d89d4e451e5f99236bb3&from=0&to=100")
+      .then(response => response.data.hits)
+  }
+
+  getFiltersVegan() {
+    return axios.get(this.BASE_URL + "?q=health=vegan&app_id=edb96150&app_key=8a70cb8c5186d89d4e451e5f99236bb3&from=0&to=100")
+      .then(response => response.data.hits)
+  }
+
+  getFiltersDairy() {
+    return axios.get(this.BASE_URL + "?q=health=dairy-free&app_id=edb96150&app_key=8a70cb8c5186d89d4e451e5f99236bb3&from=0&to=100")
+      .then(response => response.data.hits)
+  }
+
+  getFiltersShellFish() {
+    return axios.get(this.BASE_URL + "?q=health=shellfish-free&app_id=edb96150&app_key=8a70cb8c5186d89d4e451e5f99236bb3&from=0&to=100")
+      .then(response => response.data.hits)
+  }
+
+  getFiltersNuts() {
+    return axios.get(this.BASE_URL + "?q=health=tree-nut-free&health=peanut-free&app_id=edb96150&app_key=8a70cb8c5186d89d4e451e5f99236bb3&from=0&to=100")
+      .then(response => response.data.hits)
   }
 }
+
 
 
 
