ae93fbf5285841fd8c890b931ea0987c48b61a44
Rebecca Wood <rebecca@Rebeccas-MacBook-Air.local>
2019-11-28 14:03:09 +0100

    recipe images

--- public/javascripts/fridge.js
+++ public/javascripts/fridge.js
@@ -1,8 +1,8 @@
 window.onload = () => {
   const edamamAPI = new APIHandler("https://api.edamam.com/search?")
-const container = document.getElementsByClassName("fridge-container")[0]
-  
-document.getElementById("fridgeButton").onclick = () => {
+  const container = document.getElementsByClassName("fridge-container")[0]
+
+  document.getElementById("fridgeButton").onclick = () => {
 
     const ingredients = document.getElementById('ingredients').value
     edamamAPI.getIngredients(ingredients).then(recipes => {
@@ -10,11 +10,19 @@ document.getElementById("fridgeButton").onclick = () => {
       for (let i = 0; i < recipes.length; i++) { // foreach?
         container.innerHTML +=
           `<div class="recipe-info">
-      <div class="name">${recipes[i].recipe.label}</div>
-      <br>
-       <div class="url"><a href = "${recipes[i].recipe.url}"> See the full recipe </a></div>
-      </div>
-      <br> <hr> <br> `
+            <div class="img">
+              <img src="${recipes[i].recipe.image}" alt="recipeimg">
+            </div>
+            <br>
+            <div class="name">
+              <p>${recipes[i].recipe.label}</p>
+            </div>
+            <br>
+            <div class="url">
+              <a href="${recipes[i].recipe.url}"> See the full recipe </a>
+            </div>
+          </div>
+          <br> <hr> <br> `
       }
     })
   }
